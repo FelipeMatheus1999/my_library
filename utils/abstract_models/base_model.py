f7f01ebc9115f9db47ecb397e095f6f81fe7987e
Felipe Matheus <felipe_cavalcante7939@hotmail.com>
2022-12-02 12:08:47 -0300

    Adicionando "BaseModel".

---
+++ utils/abstract_models/base_model.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(
+        verbose_name=_("Date Joined"), default=timezone.now
+    )
+    date_modified = models.DateTimeField(
+        verbose_name=_("Date Modified"), default=timezone.now
+    )
+
+    class Meta:
+        abstract: True
